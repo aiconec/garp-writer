6b5b55013029c40079548f8b21aef112c8e82b2e
Safwan Samsudeen <safwansamsudeen.c@gmail.com>
2026-01-28 18:36:18 +0530

    chore: update f-ui

--- frontend/components.d.ts
+++ frontend/components.d.ts
@@ -47,7 +47,6 @@ declare module 'vue' {
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     SearchDialog: typeof import('./src/components/SearchDialog.vue')['default']
-    Select: typeof import('./src/components/Select.vue')['default']
     SpacingDialog: typeof import('./src/components/SpacingDialog.vue')['default']
     TemplateDialog: typeof import('./src/components/TemplateDialog.vue')['default']
     TextEditor: typeof import('./src/components/TextEditor.vue')['default']
